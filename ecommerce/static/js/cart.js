270b624efb9bfcc6ca9450493db30e01e6311550
ishita2108 <iwinner21@gmail.com>
2021-04-26 16:13:18 +0530

    Unauthorized User Cart View Update

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -7,13 +7,36 @@ for (i = 0; i < updateBtns.length; i++){
         console.log('productId: ', productId, 'Action: ', action)
 
         console.log('USER: ', user)
-        if (user === "AnonymousUser"){
-            console.log("Not logged in")
+        if (user == "AnonymousUser"){
+            addCookieItem(productId, action)
         }
         else {
             updateUserOrder(productId, action)
         }
     })
+}
+
+function addCookieItem(productId, action){
+    console.log("Not logged in")
+    if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {"quantity": 1}    //nested dictionary cart = { productid: {quantity:1} }
+        }
+        else{
+            cart[productId]["quantity"] += 1
+        }
+    }
+    if(action == 'remove'){
+        cart[productId]["quantity"] -= 1
+
+        if(cart[productId]["quantity"] <=0 ){
+            console.log("Remove item")
+            delete cart[productId];
+        }
+    }
+    console.log(cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
 }
 
 function updateUserOrder(productId, action){
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
+import datetime
 
 # Create your views here.
 def store(request):
@@ -28,9 +29,35 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+        print("Cart: ", cart)
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
         cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]["quantity"]
+
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]["quantity"])
+
+            order['get_cart_total'] += total
+            order['get_cart_items'] += cart[i]["quantity"]
+
+            item = {
+                'product':{
+                    'id': product.id,
+                    'name': product.name,
+                    'price': product.price,
+                    'imageURL': product.imageURL
+                },
+                'quantity': cart[i]["quantity"],
+                'get_total': total,
+            }
+            items.append(item)
 
     context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
@@ -80,3 +107,30 @@ def updateItem(request):
     # the safe parameter to False. The safe boolean parameter defaults to True. 
     # If it's set to False, any object can be passed for serialization 
     # (otherwise only dict instances are allowed).
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, create = Order.objects.get_or_create(customer = customer, complete= False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id 
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                state = data['shipping']['state'],
+                city = data['shipping']['city'],
+                zipcode = data['shipping']['zipcode']
+                )
+    else:
+        print("User is not Logged In")
+    return JsonResponse('Payment Completed', safe=False)
